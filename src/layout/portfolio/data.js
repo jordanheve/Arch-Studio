7efdc8d76d5e7af94065bdede4bad83adc8c2682
JordanHeVe <jordan.herreravera@gmail.com>
2023-11-23 21:34:01 -0600

    portfolio data

---
+++ src/layout/portfolio/data.js
@@ -0,0 +1,73 @@
+const portfolioData = [
+    {
+        name: 'Seraph Station',
+        date: 'September 2019',
+        image: '/image-seraph.jpg'
+    },
+
+    {
+        name: 'Eebox Building',
+        date: 'August 2017',
+        image: '/image-eebox.jpg'
+    },
+
+    {
+        name: '',
+        date: '',
+        image: ''
+    },
+
+    {
+        name: '',
+        date: '',
+        image: ''
+    },
+
+    {
+        name: '',
+        date: '',
+        image: ''
+    },
+
+    {
+        name: '',
+        date: '',
+        image: ''
+    },
+
+    {
+        name: '',
+        date: '',
+        image: ''
+    },
+
+    {
+        name: '',
+        date: '',
+        image: ''
+    },
+
+    {
+        name: '',
+        date: '',
+        image: ''
+    },
+
+    {
+        name: '',
+        date: '',
+        image: ''
+    },
+
+    {
+        name: '',
+        date: '',
+        image: ''
+    },
+
+    {
+        name: '',
+        date: '',
+        image: ''
+    },
+]
